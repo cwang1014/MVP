53c0a3004e93234a1a774acecf11ca721a76d100
Curtis Wang <curtis.wang10@gmail.com>
2022-10-30 16:21:10 -0700

    updated semicolons in schema to commas, that was the error I couldn't figure out

--- server/schema.sql
+++ server/schema.sql
@@ -3,9 +3,9 @@ USE mvphighscores;
 
 CREATE TABLE IF NOT EXISTS leaderboards (
   userid INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(40) NOT NULL UNIQUE DEFAULT('');
-  score BIGINT NOT NULL DEFAULT(0);
-  rows INT NOT NULL DEFAULT(0);
-  level INT NOT NULL DEFAULT(0);
-  PRIMARY KEY (userid);
+  username VARCHAR(40) NOT NULL UNIQUE,
+  score BIGINT NOT NULL,
+  rowsscore INT NOT NULL,
+  levelscore INT NOT NULL,
+  PRIMARY KEY (userid)
 );
