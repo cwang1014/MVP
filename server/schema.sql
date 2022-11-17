c88b9def886dd7a6f44f5adf3ab1d8c2d281af08
Curtis Wang <curtis.wang10@gmail.com>
2022-11-16 22:39:44 -0800

    added instructions, specifically to not press space

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS mvphighscores;
 
 CREATE TABLE IF NOT EXISTS leaderboards (
   userid SERIAL PRIMARY KEY,
-  username VARCHAR(40) NOT NULL UNIQUE,
+  username VARCHAR(40) NOT NULL,
   score BIGINT,
   rowsscore INT,
   levelscore INT
